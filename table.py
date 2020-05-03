44fcb5becfe4e6a4379fdcc761a5a69299e68e62
MaksimenkoNikolai <63637582+MaksimenkoNikolai@users.noreply.github.com>
2020-05-03 15:42:30 +0300

    Add files via upload

---
+++ table.py
@@ -0,0 +1,48 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "value=input(\"Введите номер элемента: \")\n",
+    "if value:\n",
+    "    ind=int(value)\n",
+    "    if ind == 17:\n",
+    "        print(\"Cl\")\n",
+    "    elif ind == 25:\n",
+    "        print(\"Mn\")\n",
+    "    elif ind == 80:\n",
+    "        print(\"Hg\")\n",
+    "    elif ind==3:\n",
+    "        print(\"Li\")\n",
+    "    else:\n",
+    "        print(\"Что это???\")\n",
+    "else:\n",
+    "    print(\"Введите номер элемента!\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
