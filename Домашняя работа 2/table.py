9a193d29b9500fc51f92460d239d3db074be5d42
MaksimenkoNikolai <63637582+MaksimenkoNikolai@users.noreply.github.com>
2020-06-10 23:38:47 +0300

    Update table.py

--- Домашняя работа 2/table.py
+++ Домашняя работа 2/table.py
@@ -1,1 +1,15 @@
-
+value=input("Введите номер элемента: ")
+if value:
+    ind=int(value)
+    if ind == 17:
+       print("Cl")
+    elif ind == 25:
+       print("Mn")
+    elif ind == 80:
+        print("Hg")
+    elif ind==3:
+       print("Li")
+    else:
+       print("Что это???")
+else:
+    print("Введите номер элемента!")
